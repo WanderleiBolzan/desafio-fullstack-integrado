26d69d27874e45d9bb46f4791e21dd48e4615faf
wanderlei jose bolzan <wbolzanjp@gmail.com>
2026-04-26 12:28:21 -0300

    Criada a conexão com o h2

--- backend-module/src/main/java/com/example/backend/BackendApplication.java
+++ backend-module/src/main/java/com/example/backend/BackendApplication.java
@@ -2,9 +2,14 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example") // Escaneia as entidades no ejb-module
+@EnableJpaRepositories(basePackages = "com.example") // Escaneia repositórios (se houver)
 public class BackendApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
--- backend-module/src/main/java/com/example/backend/BeneficioController.java
+++ backend-module/src/main/java/com/example/backend/BeneficioController.java
@@ -1,7 +1,11 @@
 package com.example.backend;
 
-import org.springframework.web.bind.annotation.*;
-import java.util.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/beneficios")
